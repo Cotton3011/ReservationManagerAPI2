aaf6b0d6f3db2f2a7e8436eb7f5cd8b98edf1d2a
takumi <takumi3_11@icloud.com>
2026-06-21 18:46:09 +0900

    Initial commit

---
+++ Entities/Reservation.cs
@@ -0,0 +1,20 @@
+﻿namespace ReservationManagerAPI2.Entities
+{
+	public enum ReservationStatus
+	{
+		Reserved = 0,
+		Cancelled = 1,
+	}
+	public class Reservation
+	{
+		public int Id { get; set; }
+		public int UserId { get; set; }
+		public DateTime StartTime { get; set; }
+		public DateTime EndTime { get; set; }
+		public string Memo { get; set; } = string.Empty;
+		public ReservationStatus Status { get; set; } = ReservationStatus.Reserved;
+		public DateTime CreateAt { get; set; }
+		public DateTime UpdateAt { get; set; }
+		public User? User { get; set; }
+	}
+}
---
+++ Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualBasic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ReservationManagerAPI2.Entities
+{
+	public enum UserRole
+	{
+		User = 0,
+		Admin = 1,
+	}
+
+	public class User
+	{
+		public int Id { get; set; }
+		public string UserName { get; set; } = string.Empty;
+		public string PasswordHash { get; set; } = string.Empty;
+		public UserRole Role { get; set; } = UserRole.User;
+		public DateTime CreateTime { get; set; } = DateTime.UtcNow;
+	}
+}
