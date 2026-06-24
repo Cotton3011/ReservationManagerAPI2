e13b68e4901cae892b68b618f92ecce1e0467322
takumi <takumi3_11@icloud.com>
2026-06-25 00:08:54 +0900

    service整理

--- Dtos/RegisterRequest.cs
+++ Dtos/RegisterRequest.cs
@@ -1,9 +1,13 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace ReservationManagerAPI2.Dtos
 {
 	public class RegisterRequest
 	{
+		[Required]
 		public string UserName { get; set; } = string.Empty;
+		[Required]
+		[MinLength(6)]
 		public string Password { get; set; } = string.Empty;
 	}
 }
